1b9eb9e85f05c18531986d586d05ad2cb0541f24
root <root@suresh-pc>
2023-10-23 21:38:02 +0530

    setup

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+
+require(__DIR__."/showErrors.php");
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "asme";
+
+$conn = new mysqli($hostname,$username,$password,$dbname);
+
+if($conn->connect_error){
+    die($conn->connect_error);
+}
+else{
+    echo("database connected");
+}
